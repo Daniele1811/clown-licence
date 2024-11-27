e0ececb112af4b2d7a38851d44df1a8053539201
Daniele1811 <daniele.bragalone@digital.homeoffice.gov.uk>
2024-11-27 18:20:13 +0000

    check answers and filter updates

--- app/filters.js
+++ app/filters.js
@@ -8,3 +8,12 @@ const addFilter = govukPrototypeKit.views.addFilter
 
 // Add your filters here
 
+
+addFilter('toMonth', function(x) {
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    if (x > 0) {
+      return months[x - 1]; // returns date as per month
+    } else {
+      return x;
+    }
+  })
